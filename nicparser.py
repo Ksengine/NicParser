1a3f9a35411d896dc0c230461edf891e459045cc
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2020-12-30 14:51:46 +0000

    Update nicparser.py

--- nicparser.py
+++ nicparser.py
@@ -6,7 +6,6 @@
 #  Copyright 2020 Kavindu Santhusa <kavindu@orangepiplus>
 #
 from datetime import datetime
-import re
 
 
 class Struct(object):
@@ -15,30 +14,21 @@ class Struct(object):
 
 
 def NICParser(nic):
-    nic = str(nic).lower()
-    _type = True if len(nic) == 12 else False
-    regex = r'^(\d{4})(\d{3})(\d{4})(\d{1})$' if _type else '^(\d{2})(\d{3})(\d{3})(\d{1})(\w)$'
-    matches = re.search(regex, nic)
-    year = matches.group(1) if _type else '19' + matches.group(1)
-    gender = True
+    nic = str(nic).upper()
+    nic_type = len(nic) == 12
+    year = nic[:4] if nic_type else '19' + nic[:2]
     try:
-        match = int(matches.group(2))
-        if match > 500:
-            match -= 500
-            gender = False
-        date_time = datetime.strptime('{0},{1}'.format(match, year), '%j,%Y')
+        birth_date = datetime.strptime(
+            '{0},{1}'.format(nic[4:7] if nic_type else nic[2:5], year),
+            '%j,%Y')
     except ValueError:
         raise ValueError('Birth date not within range.')
-    try:
-        special_letter=matches.group(5)
-    except IndexError:
-        special_letter=None
-    return Struct(birth_year=int(year),
-                  birth_date=date_time,
-                  gender=gender,
-                  serial_number=int(matches.group(3)),
-                  check_digit=int(matches.group(4)),
-                  special_letter=special_letter if special_letter
-                  and special_letter not in '1234567890' else None,
-                  id_type=_type,
-                  can_vote=special_letter and special_letter == 'v')
+    return Struct(
+        birth_year=int(year),
+        birth_date=birth_date,
+        gender=int(nic[4:7]) < 500 if nic_type else int(nic[2:5]) < 500,
+        serial_number=nic[5:8] if not nic_type else nic[7:11],
+        check_digit=int(nic[8]) if not nic_type else int(nic[10]),
+        special_letter=nic[-1] if nic[-1] not in '1234567890' else None,
+        id_type=nic_type,
+        can_vote=nic[-1] == 'V')
